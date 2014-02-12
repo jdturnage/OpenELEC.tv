2680958dcccbfe792aee835a599ddb036201404e
jdturnage <james.d.turnage@nasa.gov>
2014-02-12 13:50:51 -0600

    Added service.digital.signage, skin.dsign, weather, and disabled webservice addons

---
+++ projects/RPi/filesystem/usr/lib/xbmc/addons/service.digital.signage/digitalsignage.py
@@ -0,0 +1,91 @@
+import os
+import urllib2
+import xbmc
+import xbmcaddon
+from subprocess import call
+from time import sleep
+import overrides
+
+if sys.version_info < (2, 7):
+    import simplejson
+else:
+    import json as simplejson
+
+    
+#This is the detailed info that XBMC is looking for when Addon is installed.
+__addon__        = xbmcaddon.Addon(id='service.digital.signage')
+__addonid__      = __addon__.getAddonInfo('id')
+__addonversion__ = __addon__.getAddonInfo('version')
+__addonname__    = __addon__.getAddonInfo('name')
+__author__       = __addon__.getAddonInfo('author')
+__icon__         = __addon__.getAddonInfo('icon')
+__cwd__          = __addon__.getAddonInfo('path').decode("utf-8")
+__resource__   = xbmc.translatePath( os.path.join( __cwd__, 'resources', 'lib' ).encode("utf-8") ).decode("utf-8")
+
+PORT = '8124'
+
+def phoneHome():
+    #gather information such as Pi IP Address and settings information from addon
+    piip = xbmc.getIPAddress()
+    location = __addon__.getSetting("Location")
+    org = __addon__.getSetting("Org")
+    piDee = __addon__.getSetting("PiDee")
+    serverIP = __addon__.getSetting("server")
+    isolated = __addon__.getSetting("Isolated")
+    
+    pidentity = {'location' : location, 'org' : org, 'piip' : piip, 'piDee' : piDee, 'isolated' : isolated}
+    
+    data = location, piip, org, isolated
+    
+    print data
+    
+    try: 
+        req = urllib2.Request('http://' + serverIP + ':'+PORT)
+        req.add_header('Content-Type', 'application/json')
+        response = urllib2.urlopen(req, simplejson.dumps(pidentity))
+        data = response.read()
+	xbmc.log(data)
+	xbmc.log(simplejson.loads(data)['piDee'])
+	__addon__.setSetting("PiDee",str(simplejson.loads(data)['piDee']))
+    	overrides.digitalSignagePlayer.playSignage()
+    except Exception as e:
+	xbmc.log('Exception somewhere')
+	xbmc.executebuiltin('Notification(Error connecting to server,Please adjust your settings and reboot,10000)')
+	print e
+	#Go ahead and play whatever media we knew about if urllib timeouts
+	if (piDee != -1):
+		overrides.digitalSignagePlayer.playSignage()    
+
+def wait_for_internet():
+    xbmc.log('Waiting for internet')
+    while True:
+	try:
+	    response = urllib2.urlopen('http://www.nasa.gov',timeout=10)
+ 	    break
+        except urllib2.URLError:
+	    pass
+	xbmc.executebuiltin('Notification(Waiting for Internet,Trying to connect,1000)')
+	sleep(1)
+	
+    xbmc.log('Established internet')
+    xbmc.executebuiltin('Notification(Established Internet,.,1000)')
+	
+#Standard setup of main
+if (__name__ == "__main__"):
+    server = __addon__.getSetting("server")
+    xbmc.log('Version %s started' % __addonversion__)
+    xbmc.log('Digital Signage add-on is active')
+   
+    xbmc.log('Setting up the internet')
+    #Try twice to get a DHCP lease
+    if(call(["/usr/bin/nasawifi",server]) != 0):
+	xbmc.executebuiltin('Notification(Error Connecting,No DHCP Lease,1000)')
+	if(call(["/usr/bin/nasawifi"]) != 0):
+		call(["reboot"])
+
+    wait_for_internet()
+
+    #If succesful the NodeJS server will tell the Pi to play
+    #If unsuccesful the Pi will attempt to play what is in it's piFolder
+    phoneHome()
+
---
+++ projects/RPi/filesystem/usr/lib/xbmc/addons/service.digital.signage/overrides.py
@@ -0,0 +1,83 @@
+import sys
+import subprocess
+import time
+import xbmc
+import xbmcaddon
+import xbmcgui
+
+if sys.version_info < (2, 7):
+    import simplejson
+else:
+    import json as simplejson
+
+    
+#This is the detailed info that XBMC is looking for when Addon is installed.
+__addon__        = xbmcaddon.Addon(id='service.digital.signage')
+__addonid__      = __addon__.getAddonInfo('id')
+__addonversion__ = __addon__.getAddonInfo('version')
+__addonname__    = __addon__.getAddonInfo('name')
+__author__       = __addon__.getAddonInfo('author')
+__icon__         = __addon__.getAddonInfo('icon')
+__cwd__          = __addon__.getAddonInfo('path').decode("utf-8")
+
+class XBMCPlayer(xbmc.Player):
+	#Subclass of XBMC.Player.
+	# OVERRIDES: onPlayBackEnded
+	def __init__(self, *args):
+		pass
+	#def onPlayBackStopped( self ):
+	#	xbmc.log("Playback Ended")
+	#	if(xbmc.Player().isPlaying()):
+	#		xbmc.log("Not really, the video just ended")
+	#	else:
+	#		xbmc.log("Really stopped playing")
+	#		self.playSignage()
+	def playSignage(self):
+		time.sleep(3)
+		xbmc.log("Playing Media")
+		xbmc.executebuiltin('ActivateWindow(Pictures,"/media/piFolders/'+__addon__.getSetting("PiDee")+'")')
+		xbmc.executebuiltin("Action(Play)")
+
+def setPiDee(piDee):
+	try:
+		__addon__.setSetting("PiDee", piDee[1])
+		xbmc.log("PiDee set to "+piDee[1])
+		time.sleep(3)
+	except:
+		xbmc.log("Unable to set pidee to "+piDee[1])		
+def dumpSettings():
+	xbmc.log("Sending back contents of settings file")
+def playEmergency(args):
+	xbmc.log("Emergency playing")
+	#Ensure the TV is on
+	subprocess.call("echo 'on 0' | cec-client -s", shell=True)
+	xbmc.executebuiltin('ActivateWindow(Pictures,"/media/piFilling/EmergencyOverride")')
+ 	xbmc.executebuiltin("Action(Play)")
+def playIPTV(args):
+	xbmc.log("IPTV playing")
+
+digitalSignagePlayer = XBMCPlayer()
+xbmc.log("Create Digital Signage Player")
+#Standard setup of main
+if (__name__ == "__main__"):
+    xbmc.log('Version %s started' % __addonversion__)
+
+    try:
+    	if(sys.argv[1] == "piDee"):
+		setPiDee(sys.argv[1:])
+    	elif(sys.argv[1] == "play"):
+		digitalSignagePlayer.playSignage()
+	elif(sys.argv[1] == "settings"):
+		dumpSettings()
+	elif(sys.argv[1] == "emergency"):
+		playEmergency(sys.argv[1:])
+    	elif(sys.argv[1] == "iptv"):
+		playIPTV(sys.argv[1:])
+    	else:
+		xbmc.log("Unknown overrides command "+sys.argv[1])
+		xbmc.executebuiltin('Notification("Digital Signage","Unknown overrides command",5000)')
+    except:
+	xbmc.log("error understanding override")
+
+    while( not xbmc.abortRequested):
+		xbmc.sleep(100)
