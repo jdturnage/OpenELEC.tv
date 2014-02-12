5c131a565ee0e0d8973c797c4e966e40175656c1
jdturnage <james.d.turnage@nasa.gov>
2014-02-12 16:51:47 -0600

    Added distribution folder. Updated create_img script to mvoe build products to dist folder. Build products from successful build on dsbuild at 02/12/2014 4:55p.m.

--- create_img.sh
+++ create_img.sh
@@ -2,3 +2,6 @@ mv target/*.kernel target/KERNEL
 mv target/*.system target/SYSTEM
 md5sum -t target/KERNEL > target/KERNEL.md5
 md5sum -t target/SYSTEM > target/SYSTEM.md5
+
+mkdir dist
+mv target/* dist/
